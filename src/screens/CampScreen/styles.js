0a848b3dad91c554502e3fca70ff79726aa66786
Martinkovics Krisztián <muzli@wanari.com>
2018-03-19 19:49:38 +0100

    képernyők inicializálása

---
+++ src/screens/CampScreen/styles.js
@@ -0,0 +1,12 @@
+import EStyleSheet from "react-native-extended-stylesheet";
+
+export default EStyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFAD12"
+  },
+  text: {
+    flex: 1,
+    color: "#ebebeb"
+  }
+});
---
+++ src/screens/LoginScreen/LoginScreen.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import { ActionCreators } from '../../actions/actions';
+import styles from './styles';
+
+class LoginScreen extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { text: "Type here" };
+  }
+
+  componentDidMount() {
+    this.props.test('redux works');
+  }
+
+  render() {
+
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>{this.props.label}</Text>
+      </View>
+    );
+  }
+}
+
+LoginScreen.propTypes = {};
+
+const mapStateToProps = (state, props) => ({
+  label: state.SampleReducer.text
+});
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(ActionCreators, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(LoginScreen);
